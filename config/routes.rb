49fb5e54c74f78683a2773a9fc835c87461a76f2
osama <hanhan.osama@gmail.com>
2020-01-06 05:30:13 -0800

    added a bunch of variables and whatnot

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  
+  
+  get '/help', to: 'static_pages#help', as: 'help'
+  get '/contact', to: 'static_pages#contact', as: 'contact'
+  get '/login', to: 'static_pages#login', as: 'login'
+  get '/about', to: 'static_pages#about', as: 'about'
+  get '/signup', to: 'static_pages#signup', as: 'signup'
+
+
 
   get 'static_pages/home'
   get 'static_pages/about'
